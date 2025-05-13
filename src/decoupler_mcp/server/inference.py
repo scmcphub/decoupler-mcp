b69900b0abc3549cf9df535d6540b0545b54e08b
shuang <shenghui.huang@unito.it>
2025-05-13 00:33:40 +0000

    Exception process

--- src/decoupler_mcp/server/inference.py
+++ src/decoupler_mcp/server/inference.py
@@ -22,28 +22,34 @@ async def pathway_activity(
     sdtype: str = Field(default="activity", description="the saved datatype of anndata.X")
 ):
     """Pathway activity inference"""
-    result = await forward_request("infer_pathway_activity", request.model_dump())
-    if result is not None:
-        return result
-    
-    kwargs = request.model_dump()
-    progeny = dc.get_progeny(organism=kwargs["organism"], top=kwargs.get("top", None))
-    ads = ctx.request_context.lifespan_context
-    adata = ads.get_adata(dtype=dtype, sampleid=sampleid)
-    func_kwargs = filter_args(request, dc.run_mlm)
-    dc.run_mlm(mat=adata, net=progeny, **func_kwargs)
-    adata.obsm['progeny_mlm_estimate'] = adata.obsm['mlm_estimate'].copy()
-    adata.obsm['progeny_mlm_pvals'] = adata.obsm['mlm_pvals'].copy()
-    add_op_log(adata, dc.run_mlm, func_kwargs)
-    estimate_adata = obsm2adata(adata, "progeny_mlm_estimate")
-    pvals_adata = obsm2adata(adata, "progeny_mlm_pvals")
-    ads.set_adata(pvals_adata, sampleid="progeny_mlm_pvals", sdtype=sdtype)
-    ads.set_adata(estimate_adata, sampleid="progeny_mlm_estimate", sdtype=sdtype)
-    return [
-        {"sampleid": sampleid or ads.active_id, "adata": adata, "dtype": dtype},
-        {"sampleid": "progeny_mlm_pvals", "adata": pvals_adata, "dtype": sdtype},
-        {"sampleid": "progeny_mlm_estimate", "adata": estimate_adata, "dtype": sdtype}
-    ]
+    try:
+        result = await forward_request("if_pathway_activity", request, sampleid=sampleid, dtype=dtype,sdtype=sdtype)
+        if result is not None:
+            return result
+        
+        kwargs = request.model_dump()
+        progeny = dc.get_progeny(organism=kwargs["organism"], top=kwargs.get("top", None))
+        ads = ctx.request_context.lifespan_context
+        adata = ads.get_adata(dtype=dtype, sampleid=sampleid)
+        func_kwargs = filter_args(request, dc.run_mlm)
+        dc.run_mlm(mat=adata, net=progeny, **func_kwargs)
+        adata.obsm['progeny_mlm_estimate'] = adata.obsm['mlm_estimate'].copy()
+        adata.obsm['progeny_mlm_pvals'] = adata.obsm['mlm_pvals'].copy()
+        add_op_log(adata, dc.run_mlm, func_kwargs)
+        estimate_adata = obsm2adata(adata, "progeny_mlm_estimate")
+        pvals_adata = obsm2adata(adata, "progeny_mlm_pvals")
+        ads.set_adata(pvals_adata, sampleid="progeny_mlm_pvals", sdtype=sdtype)
+        ads.set_adata(estimate_adata, sampleid="progeny_mlm_estimate", sdtype=sdtype)
+        return [
+            {"sampleid": sampleid or ads.active_id, "dtype": dtype, "adata": adata},
+            {"sampleid": "progeny_mlm_pvals", "dtype": sdtype, "adata": pvals_adata},
+            {"sampleid": "progeny_mlm_estimate", "dtype": sdtype, "adata": estimate_adata}
+        ]
+    except Exception as e:
+        if hasattr(e, '__context__') and e.__context__:
+            raise Exception(f"{str(e.__context__)}")
+        else:
+            raise e    
 
 
 
@@ -56,25 +62,31 @@ async def tf_activity(
     sdtype: str = Field(default="activity", description="the saved datatype of anndata.X")
 ):
     """Transcription factor activity inference"""
-    result = await forward_request("infer_tf_activity", request.model_dump())
-    if result is not None:
-        return result
-    
-    kwargs = request.model_dump()
-    net = dc.get_collectri(organism=kwargs["organism"], split_complexes=False) 
-    ads = ctx.request_context.lifespan_context
-    adata = ads.get_adata(dtype=dtype, sampleid=sampleid)
-    func_kwargs = filter_args(request, dc.run_ulm)
-    dc.run_ulm(mat=adata, net=net, **func_kwargs)
-    adata.obsm['collectri_ulm_estimate'] = adata.obsm['ulm_estimate'].copy()
-    adata.obsm['collectri_ulm_pvals'] = adata.obsm['ulm_pvals'].copy()
-    add_op_log(adata, dc.run_ulm, func_kwargs)
-    estimate_adata = obsm2adata(adata, "collectri_ulm_estimate")
-    pvals_adata = obsm2adata(adata, "collectri_ulm_pvals")
-    ads.set_adata(pvals_adata, sampleid="collectri_ulm_pvals", sdtype=sdtype)
-    ads.set_adata(estimate_adata, sampleid="collectri_ulm_estimate", sdtype=sdtype)
-    return [
-        {"sampleid": sampleid or ads.active_id, "adata": adata, "dtype": dtype},
-        {"sampleid": "collectri_ulm_pvals", "adata": pvals_adata, "dtype": sdtype},
-        {"sampleid": "collectri_ulm_estimate", "adata": estimate_adata, "dtype": sdtype}
-    ]
+    try:
+        result = await forward_request("if_tf_activity", request, sampleid=sampleid, dtype=dtype,sdtype=sdtype)
+        if result is not None:
+            return result
+        
+        kwargs = request.model_dump()
+        net = dc.get_collectri(organism=kwargs["organism"], split_complexes=False) 
+        ads = ctx.request_context.lifespan_context
+        adata = ads.get_adata(dtype=dtype, sampleid=sampleid)
+        func_kwargs = filter_args(request, dc.run_ulm)
+        dc.run_ulm(mat=adata, net=net, **func_kwargs)
+        adata.obsm['collectri_ulm_estimate'] = adata.obsm['ulm_estimate'].copy()
+        adata.obsm['collectri_ulm_pvals'] = adata.obsm['ulm_pvals'].copy()
+        add_op_log(adata, dc.run_ulm, func_kwargs)
+        estimate_adata = obsm2adata(adata, "collectri_ulm_estimate")
+        pvals_adata = obsm2adata(adata, "collectri_ulm_pvals")
+        ads.set_adata(pvals_adata, sampleid="collectri_ulm_pvals", sdtype=sdtype)
+        ads.set_adata(estimate_adata, sampleid="collectri_ulm_estimate", sdtype=sdtype)
+        return [
+            {"sampleid": sampleid or ads.active_id, "dtype": dtype, "adata": adata},
+            {"sampleid": "collectri_ulm_pvals", "dtype": sdtype, "adata": pvals_adata},
+            {"sampleid": "collectri_ulm_estimate", "dtype": sdtype, "adata": estimate_adata}
+        ]
+    except Exception as e:
+        if hasattr(e, '__context__') and e.__context__:
+            raise Exception(f"{str(e.__context__)}")
+        else:
+            raise e    
--- src/decoupler_mcp/server/pl.py
+++ src/decoupler_mcp/server/pl.py
@@ -14,7 +14,7 @@ logger = setup_logger()
 
 
 
-pl_mcp = FastMCP("decouplerMCP-pl-Server")
+pl_mcp = FastMCP("DecouplerMCP-pl-Server")
 
 
 @pl_mcp.tool()
@@ -25,26 +25,25 @@ async def violin(
     sampleid: str = Field(default=None, description="adata sampleid for plotting")
 ):
     """Plot violin plot of one or more variables in adata.var and adata.obs or adata.obsm"""
-    result = await forward_request("pl_violin", request.model_dump())
-    if result is not None:
-        return result        
-    func_kwargs = filter_args(request, sc.pl.violin)
-    ads = ctx.request_context.lifespan_context
-    adata = ads.get_adata(dtype=dtype, sampleid=sampleid)
-    func_kwargs.pop("return_fig", True)
-    func_kwargs["show"] = False
-    func_kwargs["save"] = ".png"
-
-    try:      
+    try:
+        result = await forward_request("pl_violin", request, sampleid=sampleid, dtype=dtype)
+        if result is not None:
+            return result
+        func_kwargs = filter_args(request, sc.pl.violin)
+        ads = ctx.request_context.lifespan_context
+        adata = ads.get_adata(dtype=dtype, sampleid=sampleid)
+        func_kwargs.pop("return_fig", True)
+        func_kwargs["show"] = False
+        func_kwargs["save"] = ".png"
         fig = sc.pl.violin(adata, **func_kwargs)
-    except KeyError as e:
-        raise KeyError(f"Key {e} not found in adata.var and adata.obs")
+        fig_path = set_fig_path("violin", **func_kwargs)
+        add_op_log(adata, sc.pl.violin, func_kwargs)
+        return {"figpath": fig_path}
     except Exception as e:
-        raise e
-
-    fig_path = set_fig_path("violin", **func_kwargs)
-    add_op_log(adata, sc.pl.violin, func_kwargs)
-    return {"figpath": fig_path}
+        if hasattr(e, '__context__') and e.__context__:
+            raise Exception(f"{str(e.__context__)}")
+        else:
+            raise e
 
 
 @pl_mcp.tool()
@@ -55,27 +54,28 @@ async def matrixplot(
     sampleid: str = Field(default=None, description="adata sampleid for plotting")
 ):
     """matrixplot, Create a heatmap of the mean expression values per group of each var_names."""
-    result = await forward_request("pl_matrixplot", request.model_dump())
-    if result is not None:
-        return result
-    func_kwargs = filter_args(request, sc.pl.matrixplot)
-    ads = ctx.request_context.lifespan_context
-    adata = ads.get_adata()
-
-    func_kwargs.pop("return_fig", True)
-    func_kwargs["show"] = False
-    func_kwargs["save"] = ".png"
-
-
-    try:      
+    try:
+        result = await forward_request("pl_matrixplot", request, sampleid=sampleid, dtype=dtype)
+        if result is not None:
+            return result
+        func_kwargs = filter_args(request, sc.pl.matrixplot)
+        ads = ctx.request_context.lifespan_context
+        adata = ads.get_adata(dtype=dtype, sampleid=sampleid)
+        
+        func_kwargs.pop("return_fig", True)
+        func_kwargs["show"] = False
+        func_kwargs["save"] = ".png"
+        if request.use_obsm is not None:
+            adata = obsm2adata(adata, request.use_obsm)
         fig = sc.pl.matrixplot(adata, **func_kwargs)
-    except KeyError as e:
-        raise KeyError(f"Key {e} not found in adata.var and adata.obs.")
+        fig_path = set_fig_path("matrixplot", **func_kwargs)
+        add_op_log(adata, sc.pl.matrixplot, func_kwargs)
+        return {"figpath": fig_path}
     except Exception as e:
-        raise e
-    fig_path = set_fig_path("matrixplot", **func_kwargs)
-    add_op_log(adata, sc.pl.matrixplot, func_kwargs)
-    return {"figpath": fig_path}
+        if hasattr(e, '__context__') and e.__context__:
+            raise Exception(f"{str(e.__context__)}")
+        else:
+            raise e 
 
 
 @pl_mcp.tool()
@@ -86,28 +86,28 @@ async def embedding(
     sampleid: str = Field(default=None, description="adata sampleid for plotting")
 ):
     """Scatter plot for user specified embedding basis (e.g. umap, tsne, etc)."""
-    result = await forward_request("pl_embedding", request.model_dump())
-    if result is not None:
-        return result       
-    func_kwargs = filter_args(request, sc.pl.embedding)
-    ads = ctx.request_context.lifespan_context
-    adata = ads.get_adata()
-    
-    func_kwargs.pop("return_fig", True)
-    func_kwargs["show"] = False
-    func_kwargs["save"] = ".png"
-    if request.obsm is not None:
-        adata = obsm2adata(adata, request.obsm)
-    try:      
-        fig = sc.pl.embedding(adata, **func_kwargs)
-    except KeyError as e:
-        if request.obsm is None:
-            raise KeyError(f"Key {e} not found in adata.var and adata.obs. Please check it again, or it is in adata.obsm?")
-        else:
-            raise KeyError(f"Key {e} not found in adata.obsm")
+    try:
+        result = await forward_request("pl_embedding", request, sampleid=sampleid, dtype=dtype)
+        if result is not None:
+            return result       
+        func_kwargs = filter_args(request, sc.pl.embedding)
+        ads = ctx.request_context.lifespan_context
+        adata = ads.get_adata(dtype=dtype, sampleid=sampleid)
+        
+        func_kwargs.pop("return_fig", True)
+        func_kwargs["show"] = False
+        func_kwargs["save"] = ".png"
+        try:      
+            fig = sc.pl.embedding(adata, **func_kwargs)
+        except KeyError as e:
+            raise KeyError(f"Key '{e}' not found in adata.var and adata.obs. please check {e}, or {dtype}.")
+        except Exception as e:
+            raise e
+        fig_path = set_fig_path("embedding", **func_kwargs)
+        add_op_log(adata, sc.pl.embedding, func_kwargs)
+        return {"figpath": fig_path}
     except Exception as e:
-        raise e
-    fig_path = set_fig_path("embedding", **func_kwargs)
-    add_op_log(adata, sc.pl.embedding, func_kwargs)
-    return {"figpath": fig_path}
-
+        if hasattr(e, '__context__') and e.__context__:
+            raise Exception(f"{str(e.__context__)}")
+        else:
+            raise e
