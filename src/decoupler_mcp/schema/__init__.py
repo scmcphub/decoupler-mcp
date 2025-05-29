98c537692e82cac3b9980e2ad8a3dcb1766d1efe
shuang <shenghui.huang@unito.it>
2025-05-29 07:37:20 +0000

    add ActivityAdataInfo

--- src/decoupler_mcp/schema/__init__.py
+++ src/decoupler_mcp/schema/__init__.py
@@ -1,15 +1,8 @@
 from __future__ import annotations
 
-from .ccc import *
-from .io import *
-from .pl import *
-from scmcp_shared.schema import AdataModel
+from pydantic import BaseModel, Field, ConfigDict
 
-
-ExpAdataModel = AdataModel
-
-
-class ActivityAdataModel(AdataModel):
+class ActivityAdataInfo(BaseModel):
     """Input schema for the adata tool."""
     sampleid: str | None = Field(default=None, description="adata sampleid")
     adtype: str = Field(default="activity", description="the data type of stored adata.X, for analysis and plotting ")
